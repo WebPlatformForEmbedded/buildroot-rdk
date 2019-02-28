52542a113763d8319880a60c73ae8b44c020aab9
albertd <albert@dahan.nl>
2019-02-28 11:50:56 +0000

    [rpi-firmware] add mounting script

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -35,12 +35,31 @@ define RPI_FIRMWARE_INSTALL_TARGET_CMDS
 endef
 endif # INSTALL_VCDBG
 
+
+define RPI_FIRMWARE_MOUNT_BOOT
+	mkdir -p $(TARGET_DIR)/boot
+	grep -q '^/dev/mmcblk0p1' $(TARGET_DIR)/etc/fstab || \
+		echo -e '/dev/mmcblk0p1 /boot vfat defaults 0 0' >> $(TARGET_DIR)/etc/fstab
+endef
+
+ifeq ($(BR2_TARGET_ROOTFS_CPIO),y)
+define RPI_FIRMWARE_MOUNT_ROOT
+	mkdir -p $(TARGET_DIR)/root
+	grep -q '^/dev/mmcblk0p2' $(TARGET_DIR)/etc/fstab || \
+		echo -e '/dev/mmcblk0p2 /root ext4 defaults 0 0' >> $(TARGET_DIR)/etc/fstab
+	$(INSTALL) -m 0755 -D package/rpi-firmware/S30mountroot \
+		$(TARGET_DIR)/etc/init.d/S30mountroot
+endef
+endif
+
 define RPI_FIRMWARE_INSTALL_IMAGES_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/boot/bootcode.bin $(BINARIES_DIR)/rpi-firmware/bootcode.bin
 	$(INSTALL) -D -m 0644 $(@D)/boot/start$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).elf $(BINARIES_DIR)/rpi-firmware/start.elf
 	$(INSTALL) -D -m 0644 $(@D)/boot/fixup$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).dat $(BINARIES_DIR)/rpi-firmware/fixup.dat
 	$(INSTALL) -D -m 0644 package/rpi-firmware/config.txt $(BINARIES_DIR)/rpi-firmware/config.txt
 	$(INSTALL) -D -m 0644 package/rpi-firmware/cmdline.txt $(BINARIES_DIR)/rpi-firmware/cmdline.txt
+	$(RPI_FIRMWARE_MOUNT_BOOT)
+	$(RPI_FIRMWARE_MOUNT_ROOT)
 	$(RPI_FIRMWARE_INSTALL_DTB)
 	$(RPI_FIRMWARE_INSTALL_DTB_OVERLAYS)
 endef
