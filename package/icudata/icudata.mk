c8d6cd0b6f4565627b62700997939b31f747c3f7
albertd <albert@dahan.nl>
2019-03-28 19:30:05 +0000

    [icudata] cleaning up and make defconfig use it by default

--- package/icudata/icudata.mk
+++ package/icudata/icudata.mk
@@ -10,11 +10,11 @@ ICUDATA_SOURCE = icu4c-$(subst .,_,$(ICU_VERSION))-data.zip
 ICUDATA_SITE = http://download.icu-project.org/files/icu4c/$(ICU_VERSION)
 ICUDATA_LICENSE = ICU License
 ICUDATA_LICENSE_FILES = license.html
-ICUDATA_EXTRACT_CMDS += zip
+ICUDATA_EXTRACT_CMDS = unzip
 
 define ICUDATA_EXTRACT
     rm -rf $(@D)/source/data
-    unzip $(DL_DIR)/$(ICUDATA_SOURCE) -d $(@D)/source
+    unzip $(DL_DIR)/icudata/$(ICUDATA_SOURCE) -d $(@D)/source
     dos2unix $(@D)/source/data/Makefile.in
 endef
 
